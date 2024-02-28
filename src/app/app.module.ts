ce9e14bfb9406c17537118f436be66deb90a0725
Haziq Danial <56150665+haziqdnl@users.noreply.github.com>
2024-02-28 12:20:50 +0800

    - recommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -111,7 +111,7 @@ export function HttpLoaderFactory(http: HttpClient) {
   var appUrl = window.location.origin;
   var folderPath = "";
   if (!appUrl.includes('localhost'))  folderPath = "eqmscustomer"
-  
-  return new TranslateHttpLoader(http, folderPath + '/assets/i18n/', '.json');
+
+  return new TranslateHttpLoader(http, '/assets/i18n/', '.json');
 }
 export function tokenGetter() { return localStorage.getItem('jtwToken'); }
