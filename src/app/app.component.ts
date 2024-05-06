8fe0607553f83fe435a35f3e585e167b2b664ff6
DGSS <dgss@Patricks-MacBook-Pro.local>
2024-05-06 11:17:56 +0800

    - added configuration for APN services and OneSignal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { GeneralService } from './services/general/general.service';
+import OneSignal from 'onesignal-cordova-plugin';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,22 @@ import { GeneralService } from './services/general/general.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private g: GeneralService) {}
+  constructor(private g: GeneralService) {
+    // Remove this method to stop OneSignal Debugging
+    OneSignal.Debug.setLogLevel(6)
+    
+    // Replace YOUR_ONESIGNAL_APP_ID with your OneSignal App ID
+    OneSignal.initialize("4df23032-35d9-42b0-b50e-c2bb7c4491f7");
+
+    OneSignal.Notifications.addEventListener('click', async (e:any) => {
+      let clickData = await e.notification;
+      console.log("Notification Clicked : " + clickData);
+    })
+
+    OneSignal.Notifications.requestPermission(true).then((success: Boolean) => {
+      console.log("Notification permission granted " + success);
+    })
+  }
 
   ngOnInit() { this.g.setDefaultLanguage(); }
 }
